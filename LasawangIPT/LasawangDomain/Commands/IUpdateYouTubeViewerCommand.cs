807211b804a6a2b168491be7ff069a6cf8f2c922
Lasawang04 <144080214+Reymarklasawang@users.noreply.github.com>
2025-11-04 16:29:19 +0800

    Add files via upload

---
+++ LasawangIPT/LasawangDomain/Commands/IUpdateYouTubeViewerCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LasawangIPT101Solution.LasawangDomain.Models;
+
+namespace LasawangIPT101Solution.LasawangDomain.Commands
+{
+    public interface IUpdateYouTubeViewerCommand
+    {
+        Task Execute(YouTubeViewer youTubeViewer);
+    }
+}
---
+++ LasawangIPT/LasawangDomain/Queries/IGetAllYouTubeViewersQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LasawangIPT101Solution.LasawangDomain.Models;
+
+namespace LasawangIPT101Solution.LasawangDomain.Queries
+{
+    public interface IGetAllYouTubeViewersQuery
+    {
+        Task<IEnumerable<YouTubeViewer>> Execute();
+    }
+}
