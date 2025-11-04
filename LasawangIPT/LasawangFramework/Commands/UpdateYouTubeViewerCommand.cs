37e5f2b4bb01752903928a970aa832bbfc53ab0e
Lasawang04 <144080214+Reymarklasawang@users.noreply.github.com>
2025-11-04 16:30:51 +0800

    Add files via upload

---
+++ LasawangIPT/LasawangFramework/Commands/UpdateYouTubeViewerCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LasawangIPT101Solution.LasawangDomain.Commands;
+using LasawangIPT101Solution.LasawangDomain.Models;
+using LasawangIPT101Solution.LasawangFramework.DTOs;
+
+namespace LasawangIPT101Solution.LasawangFramework.Commands
+{
+    public class UpdateYouTubeViewerCommand : IUpdateYouTubeViewerCommand
+    {
+        private readonly YouTubeViewersDbContextFactory _contextFactory;
+
+        public UpdateYouTubeViewerCommand(YouTubeViewersDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task Execute(YouTubeViewer youTubeViewer)
+        {
+            using (YouTubeViewersDbContext context = _contextFactory.Create())
+            {
+                YouTubeViewerDto youTubeViewerDto = new YouTubeViewerDto()
+                {
+                    Id = youTubeViewer.Id,
+                    Username = youTubeViewer.Username,
+                    IsSubscribed = youTubeViewer.IsSubscribed,
+                    IsMember = youTubeViewer.IsMember,
+                };
+
+                context.YouTubeViewers.Update(youTubeViewerDto);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ LasawangIPT/LasawangFramework/Queries/GetAllYouTubeViewersQuery.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LasawangIPT101Solution.LasawangDomain.Models;
+using LasawangIPT101Solution.LasawangDomain.Queries;
+using LasawangIPT101Solution.LasawangFramework.DTOs;
+
+namespace LasawangIPT101Solution.LasawangFramework.Queries
+{
+    public class GetAllYouTubeViewersQuery : IGetAllYouTubeViewersQuery
+    {
+        private readonly YouTubeViewersDbContextFactory _contextFactory;
+
+        public GetAllYouTubeViewersQuery(YouTubeViewersDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<IEnumerable<YouTubeViewer>> Execute()
+        {
+            using (YouTubeViewersDbContext context = _contextFactory.Create())
+            {
+                IEnumerable<YouTubeViewerDto> youTubeViewerDtos = await context.YouTubeViewers.ToListAsync();
+
+                return youTubeViewerDtos.Select(y => new YouTubeViewer(y.Id, y.Username, y.IsSubscribed, y.IsMember));
+            }
+        }
+    }
+}
