cfb85bca90b070faeede78b2594bd4a9482501db
Lasawang04 <144080214+Reymarklasawang@users.noreply.github.com>
2025-11-04 16:32:59 +0800

    Add files via upload

---
+++ LasawangIPT/LasawangYoutubeViewer/ViewModels/AddYouTubeViewerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using LasawangIPT101Solution.LasawangYoutubeViewer.Commands;
+using LasawangIPT101Solution.LasawangYoutubeViewer.Stores;
+
+namespace LasawangIPT101Solution.LasawangYoutubeViewer.ViewModels
+{
+    public class AddYouTubeViewerViewModel : ViewModelBase
+    {
+        public YouTubeViewerDetailsFormViewModel YouTubeViewerDetailsFormViewModel { get; }
+
+        public AddYouTubeViewerViewModel(YouTubeViewersStore youTubeViewersStore, ModalNavigationStore modalNavigationStore)
+        {
+            ICommand submitCommand = new AddYouTubeViewerCommand(this, youTubeViewersStore, modalNavigationStore);
+            ICommand cancelCommand = new CloseModalCommand(modalNavigationStore);
+            YouTubeViewerDetailsFormViewModel = new YouTubeViewerDetailsFormViewModel(submitCommand, cancelCommand);
+        }
+    }
+}
---
+++ LasawangIPT/LasawangYoutubeViewer/ViewModels/EditYouTubeViewerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using LasawangIPT101Solution.LasawangYoutubeViewer.Commands;
+using LasawangIPT101Solution.LasawangDomain.Models;
+using LasawangIPT101Solution.LasawangYoutubeViewer.Stores;
+
+namespace LasawangIPT101Solution.LasawangYoutubeViewer.ViewModels
+{
+    public class EditYouTubeViewerViewModel : ViewModelBase
+    {
+        public Guid YouTubeViewerId { get; }
+
+        public YouTubeViewerDetailsFormViewModel YouTubeViewerDetailsFormViewModel { get; }
+
+        public EditYouTubeViewerViewModel(YouTubeViewer youTubeViewer, YouTubeViewersStore youTubeViewersStore, ModalNavigationStore modalNavigationStore)
+        {
+            YouTubeViewerId = youTubeViewer.Id;
+
+            ICommand submitCommand = new EditYouTubeViewerCommand(this, youTubeViewersStore, modalNavigationStore);
+            ICommand cancelCommand = new CloseModalCommand(modalNavigationStore);
+            YouTubeViewerDetailsFormViewModel = new YouTubeViewerDetailsFormViewModel(submitCommand, cancelCommand)
+            { 
+                Username = youTubeViewer.Username,
+                IsSubscribed = youTubeViewer.IsSubscribed,
+                IsMember = youTubeViewer.IsMember,
+            };
+        }
+    }
+}
